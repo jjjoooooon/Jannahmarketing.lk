34923e4497c8aa8172fc02551a7dbf151de43f41
sharjun hussain <sharjoonibnuhussain99@gmail.com>
2026-01-06 12:22:24 +0530

    feat:optimized the navbar

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -24,7 +24,7 @@ const Home: React.FC = () => {
             <Marquee />
             <Story />
             <Ingredients />
-            <ProductShowcase />
+            {/* <ProductShowcase /> */}
             <Testimonials />
             <BlogSection />
             <SriLankaMap />
